96d94f3408146d387f82640b0e11832fe063d69f
Stephan Richter <stephan.richter@gmail.com>
2019-05-30 13:06:34 -0400

    Provide shorter access paths to major package objects.

---
+++ src/shoobx/immutable/__init__.py
@@ -0,0 +1,6 @@
+# Re-export.
+
+from .immutable import ImmutableBase, Immutable, update
+from .immutable import ImmutableList, ImmutableSet, ImmutableDict
+from .revisioned import RevisionedImmutableBase, RevisionedImmutable
+from .revisioned import SimpleRevisionedImmutableManager, RevisionedMapping
