7078abffa1efb239fb38a338154f1f6abd509507
Stephan Richter <stephan.richter@gmail.com>
2019-05-30 16:14:48 -0400

    Back to development: 1.0.4

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ TESTS_REQUIRE = [
 
 setup (
     name="shoobx.immutable",
-    version='1.0.3',
+    version='1.0.4.dev0',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="Immutable Types",
