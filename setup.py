906ef9bc073c4e9643e2751743ca79df7cde7c72
Stephan Richter <stephan.richter@gmail.com>
2019-05-30 11:04:20 -0400

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ TESTS_REQUIRE = [
 
 setup (
     name="shoobx.immutable",
-    version='1.0.0',
+    version='1.0.1.dev0',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="A converter from RML to ODT.",
