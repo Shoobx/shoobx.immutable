625fc72cda6ac8baf278c85ebf494705420f8197
Adam Groszer <agroszer@gmail.com>
2020-02-15 08:54:30 +0100

    Back to development: 1.4.3

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ TESTS_REQUIRE = [
 
 setup (
     name="shoobx.immutable",
-    version='1.4.2',
+    version='1.4.3.dev0',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="Immutable Types",
