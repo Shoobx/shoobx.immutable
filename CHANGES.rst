78de33810b18d28e1582048744a249189dee8827
Adam Groszer <agroszer@gmail.com>
2020-02-15 08:30:16 +0100

    missed to re-export `create`

--- CHANGES.rst
+++ CHANGES.rst
@@ -6,7 +6,7 @@ CHANGES
 1.4.1 (unreleased)
 ------------------
 
-- Nothing changed yet.
+- Missed to re-export `shoobx.immutable.immutable.create`
 
 
 1.4.0 (2020-02-14)
--- src/shoobx/immutable/__init__.py
+++ src/shoobx/immutable/__init__.py
@@ -1,7 +1,7 @@
 # Re-export.
 # flake8: noqa
 
-from .immutable import ImmutableBase, Immutable, update
+from .immutable import ImmutableBase, Immutable, create, update
 from .immutable import ImmutableList, ImmutableSet, ImmutableDict
 from .revisioned import RevisionedImmutableBase, RevisionedImmutable
 from .revisioned import SimpleRevisionedImmutableManager, RevisionedMapping
